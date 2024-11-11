22f413b48ce902b9270a38e0cd58d0ec6ce25976
abdulh70 <abdulazizh70@gmail.com>
2024-11-12 00:08:21 +0300

    new chages

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -11,7 +11,7 @@ function NewPlantForm({setPlants}) {
   }
   function handleSubmit(event){
     event.preventDefault()
-    fetch('http://localhost:6001/plants',{
+    fetch('http://my-json-server.typicode.com/abdulh70/react-hooks-cc-plantshop/plants',{
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
